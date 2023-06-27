2adb2713dde74c61648f3f3619c880b50e4fc848
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-06-27 18:23:25 +0530

    Update Files

--- 1.WD_JQuery_Basic_Effects_&_Advanced/script.js
+++ 1.WD_JQuery_Basic_Effects_&_Advanced/script.js
@@ -14,15 +14,15 @@ function showTime() {
     let hour = time.getHours();
     let min = time.getMinutes();
     let sec = time.getSeconds();
-    am_pm = "AM";
+    am_pm = " AM";
 
     if (hour > 12) {
         hour -= 12;
-        am_pm = "PM";
+        am_pm = " PM";
     }
     if (hour == 0) {
         hr = 12;
-        am_pm = "AM";
+        am_pm = " AM";
     }
 
     hour = hour < 10 ? "0" + hour : hour;
