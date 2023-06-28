8f514d72fee1b660a2a88cee1ea9685330a3fa0a
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-06-28 11:54:32 +0530

    Update Files

--- 1.WD_JQuery_Basic_Effects_&_Advanced/script.js
+++ 1.WD_JQuery_Basic_Effects_&_Advanced/script.js
@@ -1,4 +1,7 @@
+// Get the current date
 const date = new Date();
+
+// Format the date to a string representation
 const fullDate = date.toLocaleString('en-US', {
     weekday: 'long',
     day: 'numeric',
@@ -6,17 +9,29 @@ const fullDate = date.toLocaleString('en-US', {
     month: 'long',
 });
 
-document.getElementById("date").innerHTML = fullDate
+// Display the formatted date in an element with id "date"
+document.getElementById("date").innerHTML = fullDate;
 
+// Function to display the current time
 function showTime() {
+    // Get the current time
     const time = new Date();
+
+    // Extract the hour, ensuring it is in 12-hour format
     const hour = (time.getHours() % 12) || 12;
-    const minute = time.getMinutes().toString().padStart(2, "0")
-    const second = time.getSeconds().toString().padStart(2, "0")
+
+    // Extract the minutes and pad with leading zero if necessary
+    const minute = time.getMinutes().toString().padStart(2, "0");
+
+    // Extract the seconds and pad with leading zero if necessary
+    const second = time.getSeconds().toString().padStart(2, "0");
+
+    // Create a string representation of the current time
     const currentTime = hour + ":" + minute + ":" + second + (hour < 12 ? " AM" : " PM");
 
+    // Display the current time in an element with id "clock"
     document.getElementById("clock").innerHTML = currentTime;
 }
 
+// Call the showTime function every second to update the displayed time
 setInterval(showTime, 1000);
-
