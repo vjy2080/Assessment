d5bd893ee7221d0a83e698f6031802874e977b5e
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-07-19 23:10:16 +0530

    Update Files

---
+++ 3.WD-Bootstrap_Basic_&_Advanced/script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Custom jQuery for the image gallery
+$(document).ready(function() {
+    // Initialize Lightbox
+    $('[data-lightbox="gallery"]').lightbox({
+      fadeDuration: 300,
+      resizeDuration: 300,
+    });
+  });
+  
