dd7b96205813caa2b767d6094a9764e18f20ff53
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-11-26 17:11:28 +0530

    Update Files

---
+++ 5.Website Designing - HTML5/script.js
@@ -0,0 +1,64 @@
+// script.js
+function validateEmail() {
+    // Validate email format
+    const email = document.getElementById('email').value;
+    const msgEmail = document.getElementById('msgEmail');
+    const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    const isValidEmail = filter.test(email);
+
+    if (!isValidEmail) {
+        msgEmail.innerHTML = "Enter a valid email address";
+    } else {
+        msgEmail.innerHTML = "";
+    }
+}
+
+function validateMobile() {
+    // Validate mobile number
+    const mobile = document.getElementById('mobile').value;
+    const msgMobile = document.getElementById("msgMobile");
+
+    if (isNaN(mobile)) {
+        msgMobile.innerHTML = "Please enter Numeric value";
+        return false;
+    }
+
+    if (mobile.length !== 10) {
+        msgMobile.innerHTML = "Enter a valid mobile number";
+        return false;
+    }
+
+    msgMobile.innerHTML = "";
+    return true;
+}
+
+
+function submit(event) {
+    // Form submission function
+    event.preventDefault();
+    console.log("called-submit");
+
+    const msgSubmit = document.getElementById("msgSubmit"); // Move this line up
+
+    const isValidEmail = validateEmail();
+    const isValidMobile = validateMobile();
+
+    if (!isValidEmail && !isValidMobile) {
+        // Additional code for form submission logic can be added here.
+        msgSubmit.innerHTML = "Please fix the validation errors before submitting.";
+        return false;
+    }
+
+    const inputs = document.querySelectorAll('form input[type="text"], form input[type="email"], form input[type="tel"], form input[type="password"],form input[type="number"],form input[type="checkbox"], form textarea');
+
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value === "") {
+            msgSubmit.innerHTML = "Please fill in all the required fields.";
+            return false;
+        }
+    }
+
+    msgSubmit.innerHTML = "Form submitted successfully!";
+}
+
+document.getElementById("register").addEventListener("click", submit);
